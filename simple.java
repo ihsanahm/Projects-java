11b6bf6a76666f066fc9c16eb11eccdbb1523bd6
ihsanahm <142923280+ihsanahm@users.noreply.github.com>
2024-02-24 23:10:41 +0500

    how much you get in 30 days
    if you are continuous double at every day in month

---
+++ simple.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class  simple
+{
+	public static void main (String args[])
+	{
+		
+		
+		int num=1;
+		
+		for (int i=1;i<31;i++)
+		{
+			System.out.println(" your continous doubling amount is "+i+" is : "+num);
+			num*=2;
+			
+		}
+		
+		
+		
+
+	}
+
+
+
+
+}
